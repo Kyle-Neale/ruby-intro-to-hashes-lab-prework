a1aa67c03b0113f4cfe3a187f3c2379431033657
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 14:51:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,9 @@ def monopoly_with_second_tier
 	monopoly = {}
   monopoly[:railroads] = {}
   monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:name] = 
+	monopoly[:railroads][:name] = {}
+	monopoly[:railroads][:rent_in_dollars] = {}
+	monopoly
 end
 
 def monopoly_with_third_tier
